3e6bb21245314eaca2e71a791462f52fd9ad26eb
Tsuikyuu <bokusummer233@gmail.com>
2025-06-06 11:04:35 +0800

    feat: hide organization management from role user

--- front_end/src/components/FullScreenSider.vue
+++ front_end/src/components/FullScreenSider.vue
@@ -3,16 +3,16 @@
     <arco-menu
       :selected-keys="[activeMenu]"
       :open-keys="expandedKeys"
-      @menu-item-click="handleMenuClick"
-      @sub-menu-click="handleSubMenuClick"
       :auto-open-selected="true"
       :auto-scroll-into-view="true"
+      @menu-item-click="handleMenuClick"
+      @sub-menu-click="handleSubMenuClick"
     >
       <arco-menu-item key="user-settings">
         <template #icon><icon-settings /></template>
         模型设置
       </arco-menu-item>
-      
+
       <arco-sub-menu key="knowledge-management">
         <template #icon><icon-folder /></template>
         <template #title>知识库管理</template>
@@ -25,12 +25,15 @@
           团队知识库
         </arco-menu-item>
       </arco-sub-menu>
-      
-      <arco-menu-item key="organization-management">
+
+      <arco-menu-item
+        v-if="userInfo?.role === 'admin' || userInfo?.role === 'superadmin'"
+        key="organization-management"
+      >
         <template #icon><icon-user /></template>
         组织管理
       </arco-menu-item>
-      <arco-menu-item key="account-logout" @click="handleLogout" >
+      <arco-menu-item key="account-logout" @click="handleLogout">
         <template #icon><icon-poweroff /></template>
         退出登录
       </arco-menu-item>
@@ -41,12 +44,12 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
-import { 
-  IconSettings,  
+import {
+  IconSettings,
   IconPoweroff,
   IconUser,
   IconFolder,
-  IconFile
+  IconFile,
 } from '@arco-design/web-vue/es/icon';
 import Cookies from 'js-cookie';
 import { useUser } from '@/store/useUser';
@@ -57,7 +60,7 @@ const router = useRouter();
 const route = useRoute();
 const { changePage } = routeController();
 
-const { setUserInfo } = useUser();
+const { setUserInfo, userInfo } = useUser();
 
 // 当前激活的菜单项
 const activeMenu = ref('personal-knowledge');
@@ -68,7 +71,7 @@ const menuRouteMap = {
   'user-settings': '/fullscreen-view/user-settings',
   'personal-knowledge': '/fullscreen-view/knowledge/personal',
   'organization-knowledge': '/fullscreen-view/knowledge/organization',
-  'organization-management': '/fullscreen-view/organization-management'
+  'organization-management': '/fullscreen-view/organization-management',
 };
 
 // 监听路由变化
@@ -100,24 +103,24 @@ const handleLogout = () => {
       // 跳转到登录页面
       changePage('/login');
       location.reload();
-    }
+    },
   });
 };
 
 // 根据当前路由设置激活的菜单项
 const initActiveMenu = () => {
   const currentPath = route.path;
-  
+
   // 反向查找路由映射
   for (const [key, path] of Object.entries(menuRouteMap)) {
     if (currentPath.includes(path)) {
       activeMenu.value = key;
-      
+
       // 如果是子菜单项，确保父菜单展开
       if (key.includes('knowledge')) {
         expandedKeys.value = ['knowledge-management'];
       }
-      
+
       break;
     }
   }
@@ -146,14 +149,14 @@ const handleSubMenuClick = (key: string) => {
 .fullscreen-sider {
   height: 100%;
   border-right: 1px solid var(--color-border);
-  
+
   :deep(.arco-menu) {
     width: 100%;
     height: 100%;
     box-shadow: none;
     border-right: none;
   }
-  
+
   :deep(.arco-menu-item) {
     height: 40px;
     line-height: 40px;
@@ -164,16 +167,16 @@ const handleSubMenuClick = (key: string) => {
       font-weight: 500;
     }
   }
-  
+
   :deep(.arco-menu-icon) {
     margin-right: 10px;
   }
-  
+
   :deep(.arco-sub-menu-title) {
     height: 40px;
     line-height: 40px;
   }
-  
+
   :deep(.arco-sub-menu-inner) {
     padding-left: 16px;
   }
